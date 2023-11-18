baa88bc710b8c0d94fcf6fe64500a568d1439ef0
zcc <132574355+zhuolhc@users.noreply.github.com>
2023-11-18 20:11:28 +0800

    Update skipping.js

--- skipping.js
+++ skipping.js
@@ -14,19 +14,24 @@
 
   const SKIP_BUTTON_SELECTOR = '.skippable-after';
   const SKIP_BUTTON_SELECTOR1 = ".cupid-pause-max-close-btn";
+  const SKIP_BUTTON_SELECTOR2 = '.ytp-ad-text.ytp-ad-skip-button-text';
 
   const CHECK_INTERVAL = 500;
 
   function skipAd() {
     const skipButton = document.querySelector(SKIP_BUTTON_SELECTOR);
     const skipButton1 = document.querySelector(SKIP_BUTTON_SELECTOR1);
-    
+    const skipButton2 = document.querySelector(SKIP_BUTTON_SELECTOR2);
+
     if (skipButton) {
       skipButton.click();
     }
     if(skipButton1){
       skipButton1.click();
     }
+    if (skipButton2) {
+        skipButton2.click();
+      }
   }
 
   setInterval(skipAd, CHECK_INTERVAL);
